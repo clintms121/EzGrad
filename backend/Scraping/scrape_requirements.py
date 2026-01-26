0e75e8a4205fa55192f0bff31cf59174a4d08220
clintms121 <clintmstapleton102@gmail.com>
2026-01-25 20:46:36 -0600

    added html scraping reference

--- backend/Scraping/scrape_requirements.py
+++ backend/Scraping/scrape_requirements.py
@@ -8,6 +8,18 @@ soup = BeautifulSoup(response.text, 'html.parser')
 course_items = soup.find_all('li', class_='acalog-course')
 course_codes = []
 
+"""
+HTML CODE BEING STRIPPED FOR REFERENCE
+<li class="acalog-course">
+  <span>
+    <a href="#" aria-expanded="false" onclick="showCourse(...); return false;">
+      POLS 1113&nbsp;-&nbsp;American Federal Government
+    </a>  
+    <strong>3</strong>
+  </span>
+</li>
+"""
+
 for item in course_items:
     a_tag = item.find('a')
     if a_tag:
